5afe4aa12b00fa83f6ff9100bd8e8ee1b5dfc431
Gabriel7H53 <gabrielhenri357@gmail.com>
2026-06-25 15:50:17 -0400

    chore: add EAS projectId to dynamic config app.config.js

--- app.config.js
+++ app.config.js
@@ -6,6 +6,9 @@ module.exports = {
     ...appConfig.expo,
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL ?? "https://proestoque-api-production-83b9.up.railway.app/api",
+      eas: {
+        projectId: "3b5174db-d890-4123-9397-ea255f151d06"
+      }
     }
   }
 };
